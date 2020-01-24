5abd834f86f3e36acf730c9f3e55f0dca427637e
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-01-24 19:25:31 -0300

    Adiciona Minion

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import NewMinion from "./containers/NewMinion";
 import NotFound from "./containers/NotFound";
 
 export default function Routes({ appProps }) {
@@ -12,6 +13,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/minions/new" exact component={NewMinion} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
