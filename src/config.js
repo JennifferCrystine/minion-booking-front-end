11c1dc5acfa0bd1aaf3e695ea707bb153c21af79
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-01-24 14:15:47 -0300

    Configurações iniciais react

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "US East (N. Virginia)",
+      BUCKET: "minion-booking-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://qre07o9c4a.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_wUiR82LSL",
+      APP_CLIENT_ID: "6grl2nu1u4ev13imcj80o3k24r",
+      IDENTITY_POOL_ID: "us-east-1:e7d069fd-02f3-4b3b-ba08-c882b1ee8f96"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Book a Minion</h1>
+        <p>Quantos Minions você quiser a um clique de distância</p>
+      </div>
+    </div>
+  );
+}
